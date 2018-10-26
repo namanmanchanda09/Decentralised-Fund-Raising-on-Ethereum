00c4faf53f9110aac307c4e3e946f16ff2773eb9
Naman Manchanda <naman.mohan09@outlook.com>
2018-10-27 02:56:40 +0530

    changed migrations code

--- migrations/2_deploy_migrations.js
+++ migrations/2_deploy_migrations.js
@@ -2,6 +2,6 @@ var Campaign = artifacts.require("./Campaign.sol");
 
 module.exports = function(deployer,accounts) {
 
-deployer.deploy(Campaign,0,web3.eth.getAccounts[0]);
+    deployer.deploy(Campaign,0,web3.eth.getAccounts[0]);
 };
 
