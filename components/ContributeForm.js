c871b871cbc7fa8f16488b19ab4cf599d929b9ac
saumya <saumyawardhan1@gmail.com>
2018-10-27 11:59:05 +0530

    Added Bool

--- components/ContributeForm.js
+++ components/ContributeForm.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import { Form, Input, Message, Button } from 'semantic-ui-react';
+import { Form, Input, Message, Button, Radio } from 'semantic-ui-react';
 import Campaign from '../ethereum/campaign';
 import web3 from '../ethereum/web3';
 import { Router } from '../routes';
@@ -8,7 +8,8 @@ class ContributeForm extends Component {
   state = {
     value: '',
     errorMessage: '',
-    loading: false
+    loading: false,
+    checkValue: 'accept'
   };
 
   onSubmit = async event => {
@@ -20,7 +21,7 @@ class ContributeForm extends Component {
 
     try {
       const accounts = await web3.eth.getAccounts();
-      await campaign.methods.contribute().send({
+      await campaign.methods.contribute(this.state.checkValue).send({
         from: accounts[0],
         value: web3.utils.toWei(this.state.value, 'ether')
       });
@@ -46,6 +47,27 @@ class ContributeForm extends Component {
           />
         </Form.Field>
 
+         <Form.Field>
+          Selected value: <b>{this.state.value}</b>
+        </Form.Field>
+        <Form.Field>
+          <Radio
+            label='Accept'
+            name='radioGroup'
+            value='this'
+            checked={this.state.checkValue === 'accept'}
+            onChange={this.handleChange}
+          />
+        </Form.Field>
+        <Form.Field>
+          <Radio
+            label='Reject'
+            name='radioGroup'
+            value='that'
+            onClick={()=>this.setState({checkValue: 'reject'})}
+            checked={this.state.checkValue === 'reject'}
+          />
+        </Form.Field>
         <Message error header="Oops!" content={this.state.errorMessage} />
         <Button primary loading={this.state.loading}>
           Contribute!
